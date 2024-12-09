021cbbaf9c741c06799d6f87b86259cdf5898e82
FaridRuano <fruanocm2777@gmail.com>
2024-12-08 20:57:39 -0500

    Set Up App

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import '@public/styles/globals.scss'
+
+export const metadata = {
+  title: "Dimar Group",
+  description: "Bienvenido al sistema de gestion empresarial de Dimar Group",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <main className="app">
+            {children}
+        </main>
+      </body>
+    </html>
+  )
+}
